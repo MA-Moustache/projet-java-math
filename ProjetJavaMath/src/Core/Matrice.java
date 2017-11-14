11e7b4a8e7f139fb80a6fc94423e92ae20a97909
MA-Moustache <michael-genique@hotmail.com>
2017-11-14 14:28:38 +0100

    Updating and deleting useless classes

--- ProjetJavaMath/src/Core/Matrice.java
+++ ProjetJavaMath/src/Core/Matrice.java
@@ -1,5 +1,16 @@
 package Core;
 
 public class Matrice {
-
+	private int nbVariables;
+	private int nbContraintes;
+	
+	public Matrice(int nbVariables, int nbContraites)
+	{
+		
+	}
+	
+	public void setVariable(int x, int y, double contenu)
+	{
+		
+	}
 }
--- ProjetJavaMath/src/Core/Simplexe.java
+++ ProjetJavaMath/src/Core/Simplexe.java
@@ -1,5 +1,18 @@
 package Core;
 
 public class Simplexe {
-
+	double z;
+	
+	double[] vb, vhb;
+	double[][] mat;
+	
+	public Simplexe(Matrice matrice)
+	{
+		
+	}
+	
+	public double[][] getMatrice()
+	{
+		return mat;
+	}
 }
