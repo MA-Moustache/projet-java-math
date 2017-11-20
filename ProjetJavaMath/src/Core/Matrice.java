282ac8c35e8b496f07afa38f00adebbf0cb83a38
ZaroSan <martin2609393@gmail.com>
2017-11-20 13:08:11 +0100

    matrice

--- ProjetJavaMath/src/Core/Matrice.java
+++ ProjetJavaMath/src/Core/Matrice.java
@@ -7,9 +7,9 @@ public class Matrice {
 
 	private double matrice[][];
 	
-	public Matrice(int x, int y)throws NumberUnderLimitException{
-		if(x>0 && y>0){
-			this.matrice=new double[x][y];
+	public Matrice(int nbContrainte, int nbVariable)throws NumberUnderLimitException{
+		if(nbContrainte>0 && nbVariable>0){
+			this.matrice=new double[nbContrainte][nbVariable];
 		}
 		else{
 			throw new NumberUnderLimitException();
@@ -31,13 +31,36 @@ public class Matrice {
 	}
 	
 	public int getNbVariables() {
-		return nbVariables;
+		return this.matrice[0].length;
 	}
 
 
 	public int getNbContraintes() {
-		return nbContraintes;
+		return this.matrice.length;
 	}
-	
+	public boolean equals(Object o){
+		if(o instanceof Matrice){
+			Matrice m=(Matrice)o;
+			for(int x=0;x<this.matrice.length;x++){
+				for(int y=0;y<this.matrice[x].length;y++){
+					if(this.matrice[x][y] != m.matrice[x][y]){
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	public String toString(){
+		String str="";
+		for(int x=0;x<matrice.length;x++){
+			for(int y=0;y<matrice[x].length;y++){
+				str+=matrice[x][y]+" ";
+			}
+			str+="\n";
+		}
+		return str;
+	}
 	
 }
