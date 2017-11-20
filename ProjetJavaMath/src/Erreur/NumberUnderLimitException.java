a220e4a8ecee7ef9772f3387417374d52e4fd9a6
ZaroSan <martin2609393@gmail.com>
2017-11-20 13:09:12 +0100

    erreur

--- ProjetJavaMath/src/Erreur/NumberUnderLimitException.java
+++ ProjetJavaMath/src/Erreur/NumberUnderLimitException.java
@@ -2,7 +2,7 @@ package Erreur;
 
 public class NumberUnderLimitException extends Exception {
 	private static final long serialVersionUID = 1L;
-	
+	//variables nulles ou negatives
 	public NumberUnderLimitException(){
 		super("Dimensions can be null or negative");
 	}
