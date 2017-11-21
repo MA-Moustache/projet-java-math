fe8ab17ac53ba717b42d1256904b620348a7b328
ZaroSan <martin2609393@gmail.com>
2017-11-21 12:20:18 +0100

    chnage

--- ProjetJavaMath/src/Core/Simplexe.java
+++ ProjetJavaMath/src/Core/Simplexe.java
@@ -1,25 +1,6 @@
 package Core;
 
 public class Simplexe {
-	double z;
 	
-	double[] vb, vhb;
-	double[][] mat;
-	
-	public Simplexe(Matrice matrice)
-	{
-		
-	}
-	
-	public double[][] getMatrice()
-	{
-		return mat;
-	}
-
-	public String toString()
-	{
-		return "VB " + vb + "\nVHB: " + vhb + "\nMatrice: " + mat;
-	}
-	///commenjlv<klhjqbmknjjhbjvi,wbetm;mkm
 	
 }
